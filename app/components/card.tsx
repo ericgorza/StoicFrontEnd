46d6c9ea7363c72b1150b01ddd10d3e95cd67957
Eric <ericgorza@gmail.com>
2024-02-15 18:10:48 -0800

    conectei o app com a API, falta adicionar fadeIn e media query

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -5,58 +5,150 @@ import Image from 'next/image';
 import styles from './card.module.css'
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Suspense } from 'react';
 import testImg from './images/Test.jpg'
+import axios from 'axios';
 
 import philosopherData from '../../config.json'
 
-const philosophers = philosopherData.philosophers;
+// const philosophers = philosopherData.philosophers;
+
+const URL_FILOSOFOS = "http://127.0.0.1:8000/filosofo/"
+const URL_PHRASES = "http://127.0.0.1:8000/phrases/"
+const URL_PICTURES = "http://127.0.0.1:8000/pictures/"
 
 
-// const randomPhilosopher = philosophers[randomIndex]
+const Card = () => {
 
-const Card = (props) => {
+    //Definir os estados que usaremos
 
-    const [mainName, setMainName] = useState("")
-    const [mainInfo, setMainInfo] = useState("")
-    const [message, setMessage] = useState("")
+    // const [mainName, setMainName] = useState("")
+    // const [mainInfo, setMainInfo] = useState("")
+    // const [message, setMessage] = useState("")
+    const [imgemFilosofo, setImagemFilosofo] = useState("")
     const [counter, setCounter] = useState(0)
+    const [imgCounter, setImgCounter] = useState(0)
+    const [phraseCounter, setPhraseCounter] = useState(0)
 
-    const changeStates = (counter) => {
-        setMainName(philosophers[counter]['name']);
-        setMainInfo(philosophers[counter]['date']);
-        setMessage(philosophers[counter]['message']);
-    }
+    //Criar o estado para CSS Fade In:
+    // const [fadeIn, setFadeIn] = useState(false);
+
+    // const changeStates = () => {
+    //     setFadeIn(false); // Ative o efeito fadeIn
+    //     setTimeout(() => {
+    //       setFadeIn(true); // Desative o efeito fadeIn após algum tempo (ajuste conforme necessário)
+    //     }, 1000); // Tempo em milissegundos, ajuste conforme necessário
+    //   };
+
+    //Fazer o fetch na API
+
+    const [dados, setDados] = useState([])
+    const [phrases, setPhrases] = useState([])
+    const [images, setImages] = useState([])
+    const [dict, setDict] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const responseFilosofo = await axios.get(URL_FILOSOFOS);
+            setDados(responseFilosofo.data);
+            const responsePhrase = await axios.get(URL_PHRASES)
+            setPhrases(responsePhrase.data)
+            const responseImage = await axios.get(URL_PICTURES)
+            setImages(responseImage.data)
+
+          } catch (error) {
+            console.error('Erro ao obter dados da API:', error);
+          }
+        };
+
+        fetchData();
+      }, []);
+
+    useEffect(() => {
+
+        const organizedData = dados.map((item) => {
+          const nomeFilosofo = item.name;
+          const locationFilosofo = item.location;
+          const frasesFilosofo = phrases.filter((phrase) => phrase.philosopher === item.id).map((item) => (item.phrase));
+          const imgFilosofo = images.filter((i) => i.philosopher === item.id).map((item) => (item.img_url));
+
+          return {
+            nomeFilosofo,
+            locationFilosofo,
+            frasesFilosofo,
+            imgFilosofo,
+          };
+        });
+
+        // Atualiza o estado dict com os novos dados organizados
+        setDict(organizedData);
+        console.log(dict)
+        const filosofoUm = dict[0]
+        if(filosofoUm){
+            console.log(filosofoUm.nomeFilosofo)
+        }
+    }, [dados,phrases,images])
+
+// Descomente se voce quiser utilizar a fake API:
+
+    // const changeStates = (counter) => {
+    //     setMainName(philosophers[counter]['name']);
+    //     setMainInfo(philosophers[counter]['date']);
+    //     setMessage(philosophers[counter]['message']);
+    // }
+
+// Descomente se voce quiser utilizar a API Django:
+
+//     const changeStates = (counter) => {
+//         if(dict[counter]){
+//             // setMainName(dict[counter].nomeFilosofo);
+//             // setMainInfo(dict[counter].locationFilosofo);
+//             // setMessage(dict[counter].frasesFilosofo[phraseCounter]);
+//             setImagemFilosofo(dict[counter].imagemFilosofo[imgCounter]);
+
+// }}
 
     const handleClick = (e) => {
         e.preventDefault()
-        console.log(counter)
-        console.log(philosophers.length)
-        if(counter >= (philosophers.length - 1)){
+        console.log("Counter: ", counter)
+
+        let randomPhraseIndex = Math.floor(Math.random() * 4);
+        let randomImgIndex = Math.floor(Math.random() * 2);
+
+        setImgCounter(randomImgIndex)
+        setPhraseCounter(randomPhraseIndex)
+        // changeStates()
+
+        console.log("Img Counter: ", imgCounter)
+        console.log("Phrase Counter: ", phraseCounter)
+
+        if(counter >= (dict.length - 1)){
             setCounter(0)
-            changeStates(counter)
+            // changeStates(counter)
         } else {
             setCounter(counter + 1);
-            changeStates(counter)
+            // changeStates(counter)
     }}
 
     useEffect(() =>{
-        changeStates(0)
+        // changeStates(0)
         setCounter(counter + 1);
+        // setImgCounter(imgCounter + 1)
+        // setPhraseCounter(phraseCounter + 1)
     },[])
 
 
 
   return (
     <main className="main-page">
-        {mainName.length > 0 && mainInfo.length > 0 && message.length > 0?
+        {dict[counter]?
             <div className={styles.cardContainer}>
                 <div className={styles.textContainer}>
-                    <h1 className={styles.mainName}>{mainName}</h1>
-                    <p className={styles.mainInfo}>{mainInfo}</p>
+                    <h1 className={styles.mainName}>{dict[counter].nomeFilosofo}</h1>
+                    <p className={styles.mainInfo}>{dict[counter].locationFilosofo}</p>
                     <div className={styles.messageContainer}>
                         <p className={styles.mainMessage}>
-                            {message}
+                            {dict[counter].frasesFilosofo[phraseCounter]}
                         </p>
                     </div>
                 </div>
@@ -75,7 +167,6 @@ const Card = (props) => {
                 <h1>Loading...</h1>
             </div>
             }
-        {/* </Suspense> */}
             <div className={styles.buttonContainer}>
                     <button className={styles.button} onClick={handleClick}>
                         <span className="buttonSpan">Next</span>
--- app/page.tsx
+++ app/page.tsx
@@ -5,73 +5,16 @@ import Link from "next/link";
 import Header from "./components/header";
 import Card from "./components/card";
 import Footer from "./components/footer";
-import Api from "./components/api";
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const URL_FILOSOFOS = "http://127.0.0.1:8000/filosofo/"
-const URL_PHRASES = "http://127.0.0.1:8000/phrases/"
-const URL_PICTURES = "http://127.0.0.1:8000/pictures/"
-
+import Api from "./components/apitest";
 
 
 import "./page.css";
 
 export default function Page() {
-
-    const [dados, setDados] = useState([])
-    const [phrases, setPhrases] = useState([])
-    const [images, setImages] = useState([])
-    const [dict, setDict] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const responseFilosofo = await axios.get(URL_FILOSOFOS);
-            setDados(responseFilosofo.data);
-            const responsePhrase = await axios.get(URL_PHRASES)
-            setPhrases(responsePhrase.data)
-            const responseImage = await axios.get(URL_PICTURES)
-            setImages(responseImage.data)
-
-          } catch (error) {
-            console.error('Erro ao obter dados da API:', error);
-          }
-        };
-
-        fetchData();
-      }, []);
-
-    useEffect(() => {
-        console.log('Dados:', dados);
-        console.log('Phrases:', phrases);
-        console.log('Images:', images);
-
-        const organizedData = dados.map((item) => {
-          const name = item.name;
-          const location = item.location;
-          const frases = phrases.filter((phrase) => phrase.philosopher === item.id).map((item) => (item.phrase));
-          const img = images.filter((i) => i.philosopher === item.id).map((item) => (item.img_url));
-
-          return {
-            name,
-            location,
-            frases,
-            img,
-          };
-        });
-
-        // Atualiza o estado dict com os novos dados organizados
-        setDict(organizedData);
-        console.log(dict)
-    }, [dados,phrases,images])
-
-
-
     return (
     <div>
         <Header />
-        <Card filosofo={dict}/>
+        <Card/>
         <Footer />
         {/* <div style={{width:"400px"}}>
             <Api />
